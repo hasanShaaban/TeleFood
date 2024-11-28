c256129e1ffc1ab5db30aae9bbe92c97f70854aa
hasanShaaban <152873746+hasanShaaban@users.noreply.github.com>
2024-11-28 10:12:53 -0800

    first commit

---
+++ lib/core/utils/constant.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const kPrimeryColor =              Color(0xff2A2C41);
+const kScaffoldColor =             Color(0xffF4F4F8);
+const kSecondaryColor =            Color(0xffFF724C);
+const kTextColor =                 Color(0xffFDBF50);
