c7fd6809f15929b0ee860ae8f531ff09a42fbfc5
hasanShaaban <152873746+hasanShaaban@users.noreply.github.com>
2024-11-28 16:11:47 -0800

    creat sliverappbar and put the color and border radius

--- lib/featuers/home/presentation/views/home_view.dart
+++ lib/featuers/home/presentation/views/home_view.dart
@@ -1,10 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:telefood/core/utils/constant.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(child: Scaffold());
+    return const SafeArea(child: Scaffold(body: HomeViewBody(),));
+  }
+}
+
+class HomeViewBody extends StatelessWidget {
+  const HomeViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const  CustomScrollView(
+      slivers: [
+        SliverAppBar(
+          expandedHeight: 200,
+          collapsedHeight: 100,
+          backgroundColor: kPrimeryColor,
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.only(bottomLeft: Radius.circular(38), bottomRight: Radius.circular(38))),
+        )
+      ],
+    );
   }
 }
--- lib/featuers/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/featuers/splash/presentation/views/widgets/splash_view_body.dart
@@ -33,7 +33,7 @@ class _SplashViewBodyState extends State<SplashViewBody> {
 
   void navigateToHome() {
     Future.delayed(const Duration(seconds: 5), (){
-      GoRouter.of(context).push(AppRouter.kHomeViewRouter);
+      GoRouter.of(context).pushReplacement(AppRouter.kHomeViewRouter);
     });
   }
 
