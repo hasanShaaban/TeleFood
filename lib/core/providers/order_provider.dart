232fe5803aac40be748f62f092c0a2a41ea9ea29
hasanShaaban <hasan2005shaaban@gmail.com>
2025-01-11 23:58:20 +0300

    commit 2

--- lib/core/providers/order_provider.dart
+++ lib/core/providers/order_provider.dart
@@ -1,1 +1,22 @@
+import 'package:flutter/material.dart';
 
+class OrderProvider extends ChangeNotifier{
+  int? payId;
+  String? location;
+  int? tips;
+
+  OrderProvider();
+
+  void setPayId(int newPayId) async{
+    payId = newPayId;
+    notifyListeners();
+  }
+  void setLocation(String newLocation) async{
+    location = newLocation;
+    notifyListeners();
+  }
+  void setTips(int newTips) async{
+    tips = newTips;
+    notifyListeners();
+  }
+}
--- lib/featuers/cart/presentation/views/cart_view.dart
+++ lib/featuers/cart/presentation/views/cart_view.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:telefood/core/providers/order_provider.dart';
 import 'package:telefood/featuers/cart/data/models/cart_response/cart_response.dart';
 import 'package:telefood/featuers/cart/presentation/views/widgets/cart_view_body.dart';
+
 
 class CartView extends StatelessWidget {
   const CartView({super.key, required this.cartinfo});
@@ -9,11 +12,18 @@ class CartView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-        child: Scaffold(
-      body: CartViewBody(
-        cartinfo: cartinfo,
-      ),
-    ));
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => OrderProvider()),
+      ],
+      child: SafeArea(
+          child: Scaffold(
+        body: CartViewBody(
+          cartinfo: cartinfo,
+        ),
+      )),
+    );
   }
 }
+
+
